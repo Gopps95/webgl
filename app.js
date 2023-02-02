81c9b34366a108829ef120afce20fa616631ac81
gopps95 <shankargopi7@gmail.com>
2023-02-02 18:46:33 +0530

    first_js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 var initDem = function() {
+    console.log('The hunt begins');
+    var canvas = document.getElementById('mycanvas');
+    var gl = canvas.getContext('webgl');
+    gl.clearColor(0.75,0.85,0.8,1.0);
+    gl.clear();
 
 };
